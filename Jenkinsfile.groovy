700d8c2871339e0c544a242ebabf146faab66550
abhishekgund <91332903+abhishekgund@users.noreply.github.com>
2026-03-03 15:00:11 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,22 @@
 pipeline {
     agent any
-    
     stages {
-        stage('Pull Develop Content') {
+        stage('Checkout Develop Branch') {
             steps {
-                // Task 2: Pull 'develop' branch content into 'my-project-folder'
-                dir('my-project-folder') {
-                    checkout scm
-                }
+                // Task 2: Force checkout into 'my-project-folder'
+                checkout([
+                    $class: 'GitSCM', 
+                    branches: [[name: '*/develop']], 
+                    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'my-project-folder']], 
+                    userRemoteConfigs: [[url: 'YOUR_GIT_REPO_URL', credentialsId: 'YOUR_CRED_ID']]
+                ])
             }
         }
-        
-        stage('Verify Pull') {
+        stage('Verify Files') {
             steps {
-                // Verify the folder was created and contains files
-                sh 'ls -R my-project-folder'
+                // This step MUST appear in your console logs
+                echo "Checking contents of my-project-folder..."
+                sh 'ls -la my-project-folder' 
             }
         }
     }
